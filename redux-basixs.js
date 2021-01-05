0507a96bc1e17a0af17d45e5a753d8bc7ef32318
Hoshitter1 <hoshi.to.318@gmail.com>
2021-01-05 15:08:43 +0900

    Invalid hook call

--- redux-basixs.js
+++ redux-basixs.js
@@ -1,39 +1,39 @@
-const redux = require("redux");
-const createStore = redux.createStore;
+// const redux = require("redux");
+// const createStore = redux.createStore;
 
-const initalState = {
-  counter: 0,
-};
+// const initalState = {
+//   counter: 0,
+// };
 
-// Reducer
-const rootReducer = (state = initalState, action) => {
-  if (action.type == "INC_COUNTER") {
-    return {
-      ...state,
-      counter: state.counter + 1, // Always do this way
-    };
-  }
-  if (action.type == "ADD_COUNTER") {
-    return {
-      ...state,
-      counter: state.counter + action.value, // Always do this way
-    };
-  }
-  return state;
-};
+// // Reducer
+// const rootReducer = (state = initalState, action) => {
+//   if (action.type == "INC_COUNTER") {
+//     return {
+//       ...state,
+//       counter: state.counter + 1, // Always do this way
+//     };
+//   }
+//   if (action.type == "ADD_COUNTER") {
+//     return {
+//       ...state,
+//       counter: state.counter + action.value, // Always do this way
+//     };
+//   }
+//   return state;
+// };
 
-// Store
-const store = createStore(rootReducer);
-console.log(store.getState());
+// // Store
+// const store = createStore(rootReducer);
+// console.log(store.getState());
 
-// Subscription
-// This gets triggered whenever state is updated
-store.subscribe(() => {
-  console.log("[subscription]", store.getState());
-});
+// // Subscription
+// // This gets triggered whenever state is updated
+// store.subscribe(() => {
+//   console.log("[subscription]", store.getState());
+// });
 
-// Dispayching action
-store.dispatch({ type: "INC_COUNTER" });
-console.log(store.getState());
-store.dispatch({ type: "ADD_COUNTER", value: 10 });
-console.log(store.getState());
+// // Dispayching action
+// store.dispatch({ type: "INC_COUNTER" });
+// console.log(store.getState());
+// store.dispatch({ type: "ADD_COUNTER", value: 10 });
+// console.log(store.getState());
